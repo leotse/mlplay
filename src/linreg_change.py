99adabf35f56fd1159ae03ffcb746bcf0a531bfa
Leo Tse <leocltse@gmail.com>
2017-03-01 22:22:32 -0500

    Fixed degree polynomial.

--- src/linreg_change.py
+++ src/linreg_change.py
@@ -4,8 +4,8 @@ model looks at the n days history of price change, then predicts
 next days price change; scoring does not care about the magnitude
 of the change but only the direction of the change (+ or -)
 
-update #1 - added argument to predict n days in the future instead of 
-always looking at the immediate next day; accuracy is surprisingly high 
+update #1 - added argument to predict n days in the future instead of
+always looking at the immediate next day; accuracy is surprisingly high
 but this is probably just caused by the bias of picking well known stocks
 """
 
@@ -18,14 +18,17 @@ from data.loader import load, load_history
 from data.splitter import split
 
 # args
-TICKER = 'AAPL'
+TICKER = 'FB'
 DAYS_HISTORY = 252
 DAYS_FUTURE = 252
-DEGREE = 0
+DEGREE = 1
 ALPHA = 3.0
 
 # load price data
 df = load(TICKER)
+
+# we only want to look at the most recent 5 years of data
+df = df[-252 * 5:]
 
 # load price change history
 # collapses n days history into one row
@@ -42,9 +45,6 @@ change_future.name = 'change_future'
 
 # line up data and drop any na values
 X = df_change_history.join(df_volume_history).join(change_future).dropna()
-
-# we only want to look at the most recent 5 years of data
-# X = X[-255*5:]
 
 # shuffle data frame
 X = X.reindex(np.random.permutation(X.index))
